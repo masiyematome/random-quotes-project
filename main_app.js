fe37b0aedbb47ff29abbc059e85237a3fd33b6b2
masiyematome <matomemasiye7@gmail.com>
2021-01-12 09:01:24 +0200

    Created an array of objects containing quotes and authors

--- main_app.js
+++ main_app.js
@@ -15,3 +15,58 @@ function generateRandomQuote(quotes){
     console.log(quotes[randomIndex]);
 }
 
+function displayQuote(theQuotes){
+    const theQuotes = [
+        {
+            quote: "The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees Opportunity In Every Difficulty.",
+            author : "Winston Churchill",
+        },
+
+        {
+            quote: "You Learn More From Failure Than From Success. Don’t Let It Stop You. Failure Builds Character.",
+            author: "Samuel Smiles",
+        },
+
+        {
+            quote: "If You Are Working On Something That You Really Care About, You Don’t Have To Be Pushed. The Vision Pulls You.",
+            author : "Steve Jobs",
+        },
+
+        {
+            quote: "People Who Are Crazy Enough To Think They Can Change The World, Are The Ones Who Do.",
+            author: "Rob Siltanen",
+        },
+
+        {
+            quote: "Entrepreneurs Are Great At Dealing With Uncertainty And Also Very Good At Minimizing Risk. That’s The Classic Entrepreneur.",
+            author : "Mohnish Pabrai",
+        },
+
+        {
+            quote: "Knowing Is Not Enough; We Must Apply. Wishing Is Not Enough; We Must Do.",
+            author: "Johann Wolfgang Von Goethe",
+        },
+
+        {
+            quote: "What You Lack In Talent Can Be Made Up With Desire, Hustle And Giving 110% All The Time.",
+            author : "Don Zimmer",
+        },
+
+        {
+            quote: "Develop An ‘Attitude Of Gratitude’. Say Thank You To Everyone You Meet For Everything They Do For You.",
+            author: "Brian Tracy",
+        },
+
+        {
+            quote: "Fake It Until You Make It! Act As If You Had All The Confidence You Require Until It Becomes Your Reality.",
+            author : "Brian Tracy",
+        },
+
+        {
+            quote: "For Every Reason It’s Not Possible, There Are Hundreds Of People Who Have Faced The Same Circumstances And Succeeded.",
+            author: "Jack Canfield",
+        },
+
+    ];
+}
+
